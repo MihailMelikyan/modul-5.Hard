cd4760268b10e2059996519979e887dea6ecb705
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-08-14 21:51:21 +0300

    исправил код

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class User:
         self.password = hash(password)
         self.age = age
 
-    def __str__(self, nickname):
+    def __str__(self):
         return self.nickname
 
     def get_nickname(self):
@@ -15,14 +15,14 @@ class User:
 
 
 class Video:
-    def __init__(self, title, duration, time_now, adult_mode):
+    def __init__(self, title, duration, adult_mode = False):
         self.title = title
         self.duration = duration
-        self.time_now = time_now
+        self.time_now = 0
         self.adult_mode = adult_mode
-        adult_mode = False
 
-    def __str__(self, title):
+
+    def __str__(self):
         return self.title
 
 
@@ -34,44 +34,33 @@ class UrTube:
 
     def log_in(self, login, password):
         if not self.current_user is None:
-            print('Вы уже в системе ! ')
+            print('Вы уже в системе !')
+            return
         elif self.current_user is None:
             for user in self.users:
                 if user.nickname == login:
                     if user.password == hash(password):
                         self.current_user = user
                         print(f'Вы вошли в систему {user.nickname}')
-                        break
+                        return
             else:
                 print('Такой пользователь не найден !')
 
     def register(self, nickname, password, age):
         for user in self.users:
             if user.nickname == nickname:
-                if user.password == password:
-                    self.current_user = user
-                    print(f'Пользователь {nickname} уже существует')
-        else:
-            new_user = User(nickname, password, age)
-            self.users.append(new_user)
-            print(f'Создал нового пользователя {nickname}')
+                print(f'Пользователь {nickname} уже существует')
+                return
 
-    def log_out(self, User):
-        self.current_user = None
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        print(f'Создал нового пользователя {nickname}')
+        self.current_user = new_user
 
     def add(self, *all_videos):
-        list = []
-        if self.current_user is None:
-            print('Ошибка')
-        else:
-            for i in all_videos:
-                if i not in self.videos:
-                    list.append(i)
-                if len(list) > 0:
-                    self.videos.extend(list)
-                    print('Добавил' * list, 'в видео')
-                else:
-                    print('Такие видео уже есть!')
+        for video in all_videos:
+            if video not in self.videos:
+                self.videos.append(video)
 
     def get_videos(self, word):
         word = word.lower()
@@ -81,7 +70,7 @@ class UrTube:
             if word in obj:
                 list_w.append(i.title)
                 if len(list_w) > 0:
-                    print('Нашел похожее видео -' * list_w)
+                    print('Нашел похожее видео -', list_w)
                 else:
                     print('Нет ничего похожего')
 
@@ -90,20 +79,21 @@ class UrTube:
             for i in self.videos:
                 if name_video == i.title:
                     if self.current_user.age >= 18:
-                        for j in range(i.time_now, i.duration):
-                            print(self.current_user.age)
-                            print(f'Просмотр фильма {i.title} : {j} из {i.duration}')
-                            i.time_now = j
+                        while i.time_now <= i.duration:
+                            print(f'{i.time_now}', end=' ')
+                            i.time_now += 1
                             time.sleep(1)
                     else:
                         print('Видео с ограничениями по возрасту ,покиньте пожалуйста страницу!')
         else:
             print('Вы не авторизованы ,войдите пожалуйтса в аккаунт ! ')
 
+
+
 if __name__ == '__main__':
     ur = UrTube()
-    v1 = Video('Лучший язык программирования 2024 года', 200,0,False)
-    v2 = Video('Для чего девушкам парень программист?', 10,0,True)
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode := True)
     # Добавление видео
     ur.add(v1, v2)
 
